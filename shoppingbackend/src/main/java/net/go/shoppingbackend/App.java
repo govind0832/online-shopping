97ca2f782766ddf5b27777058ba87274bfbe7720
go <gr0832@gmail.com>
2021-03-06 15:36:17 +0530

    Project setup completed

---
+++ shoppingbackend/src/main/java/net/go/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.go.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
